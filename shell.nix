b721d8847c6dcbd36d5a7e9e0352a495c40e8f43
0nyr <rascoussier.florian@gmail.com>
2023-08-17 14:48:50 +0200

    Added basic nix shell config.

---
+++ shell.nix
@@ -0,0 +1,36 @@
+{ pkgs ? import <nixpkgs> {} }:
+
+pkgs.mkShell {
+  buildInputs = [
+    pkgs.texlive.combined.scheme-full
+    pkgs.biber
+  ];
+  # buildInputs = with pkgs.texlive; [
+  #   # General Tools
+  #   combined.scheme-full
+
+  #   # Specific Packages
+  #   babel
+  #   csquotes
+  #   dirtytalk
+  #   fontenc
+  #   geometry
+  #   glossaries
+  #   graphicx
+  #   hyperref
+  #   inputenc
+  #   listings
+  #   multicol
+  #   multirow
+  #   scrlayer-scrpage
+  #   setspace
+  #   tocbasic
+
+  #   # Biber and Biblatex for bibliography management
+  #   biber
+  #   biblatex
+
+  #   # Utilities
+  #   latexmk
+  # ];
+}
