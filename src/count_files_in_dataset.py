0905578375d457f079fe1ebf9dd3fce75d5c422e
0nyr <rascoussier.florian@gmail.com>
2023-10-11 17:24:24 +0200

    Added empty subdir filtering in count script.

--- src/count_files_in_dataset.py
+++ src/count_files_in_dataset.py
@@ -16,31 +16,38 @@ def count_raw_files_in_subdir(subdir):
 def main(dataset_dir):
     print("INPUT DIR:", dataset_dir)
     subdirs_raw_count = {}
+    empty_subdirs = []
 
     for subdir, _, _ in os.walk(dataset_dir):
         if not any(os.path.isdir(os.path.join(subdir, sub_subdir)) for sub_subdir in os.listdir(subdir)):
             # Only consider the lowest level subdirectories
             raw_count = count_raw_files_in_subdir(subdir)
-            subdirs_raw_count[subdir] = raw_count
+            if raw_count > 0:
+                subdirs_raw_count[subdir] = raw_count
+            else:
+                empty_subdirs.append(subdir)
 
     if not subdirs_raw_count:
         print("No subdirectories with RAW files found.")
         return
 
+    print(f"Number of empty subdirectories: {len(empty_subdirs)}")
+
     raw_counts = list(subdirs_raw_count.values())
-    
+
     # Compute statistics
     median = statistics.median(raw_counts)
-    print(statistics.quantiles(raw_counts, n=4))
-    first_quartile = statistics.quantiles(raw_counts, n=4)[0]
-    fourth_quartile = statistics.quantiles(raw_counts, n=4)[2]
-    
+    quartiles = statistics.quantiles(raw_counts, n=4)
+    print(quartiles)
+    first_quartile = quartiles[0]
+    fourth_quartile = quartiles[2]
+
     print(f"Median number of RAW files: {median}")
 
     # Identify subdirs in first and fourth quartiles
     first_quartile_subdirs = [subdir for subdir, count in subdirs_raw_count.items() if count <= first_quartile]
     fourth_quartile_subdirs = [subdir for subdir, count in subdirs_raw_count.items() if count >= fourth_quartile]
-    
+
     print("\nSubdirectories in the first quartile:")
     for subdir in first_quartile_subdirs:
         print(subdir, "with", subdirs_raw_count[subdir], "RAW files")
